1479a964dcf3616aafe3d0cf1bf5e4e85a752cb4
Trevor Turk <trevorturk@gmail.com>
2021-03-09 11:00:40 -0600

    rm comments, move to PR description

--- rack-async-http-falcon-async-await/app.rb
+++ rack-async-http-falcon-async-await/app.rb
@@ -1,6 +1,3 @@
-# Pulling from the example here:
-# https://github.com/socketry/async-await/blob/00052e12a80ff02b3821222c52a8aed5f1c99799/examples/sleep_sort.rb
-
 require "async/await"
 
 class App
@@ -14,22 +11,6 @@ class App
 
       puts "Request finish"
 
-      # Logs:
-      # Request start
-      # Waiting at the barrier!
-      # I've sorted 2 for you.
-      # I've sorted 2 for you.
-      # I've sorted 3 for you.
-      # I've sorted 4 for you.
-      # I've sorted 5 for you.
-      # Returning the result
-      # Request finish
-      # I've sorted 5 for you.
-      # I've sorted 7 for you.
-      # I've sorted 8 for you.
-      # I've sorted 9 for you.
-
-      # Returns [2,2,3,4,5]
       [200, {}, [results.to_json]]
     end
 
@@ -47,7 +28,6 @@ class App
         sort_one(item, result)
       end
 
-      # Wait until all previous async method calls have finished executing.
       puts "Waiting at the barrier!"
       barrier!
 
@@ -56,49 +36,3 @@ class App
     end
   end
 end
-
-# WIP trying to get something along these lines working...
-
-# require "async/http/internet"
-# require "async/await"
-#
-# class App
-#   class << self
-#     include Async::Await
-#
-#     sync def call(env)
-#       puts "Request start"
-#
-#       result = {}
-#
-#       delay_1_data
-#       delay_2_data
-#       barrier!
-#
-#       result.merge!(one: delay_1_data["url"])
-#       result.merge!(two: delay_2_data["url"])
-#       result.merge!(three: delay_2_data["url"])
-#
-#       raise result.inspect
-#
-#       puts "Request finish"
-#
-#       [200, {}, [result.to_json]]
-#     end
-#
-#     async def fetch_data(url)
-#       internet = Async::HTTP::Internet.new
-#       return JSON.parse(internet.get(url).read)
-#     ensure
-#       internet&.close
-#     end
-#
-#     async def delay_1_data
-#       @_delay_1_data ||= fetch_data("https://httpbin.org/delay/1")
-#     end
-#
-#     async def delay_2_data
-#       @_delay_2_data ||= fetch_data("https://httpbin.org/delay/2")
-#     end
-#   end
-# end
