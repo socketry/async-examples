7be7c5e917a2ed092747403082a5cc6282ff3f87
Trevor Turk <trevorturk@gmail.com>
2021-03-09 11:31:53 -0600

    try to improve it, but break it

--- rack-async-http-falcon-graphql-lazy-resolve/query.rb
+++ rack-async-http-falcon-graphql-lazy-resolve/query.rb
@@ -6,23 +6,32 @@ class Query < GraphQL::Schema::Object
   field :three, String, null: false
 
   def one
-    Async do
-      response = Async::HTTP::Internet.new.get("https://httpbin.org/delay/1").read
-      JSON.parse(response)["url"]
-    end
+    delay_1_data["url"]
   end
 
   def two
-    Async do
-      response = Async::HTTP::Internet.new.get("https://httpbin.org/delay/2").read
-      JSON.parse(response)["url"]
-    end
+    delay_2_data["url"]
   end
 
   def three
-    Async do
-      response = Async::HTTP::Internet.new.get("https://httpbin.org/delay/2").read
-      JSON.parse(response)["url"]
+    delay_2_data["url"]
+  end
+
+  def internet
+    @_internet ||= Async::HTTP::Internet.new
+  end
+
+  def delay_1_data
+    @_delay_1_data ||= Async do
+      puts "getting delay_1_data"
+      JSON.parse(internet.get("https://httpbin.org/delay/1").read)
+    end
+  end
+
+  def delay_2_data
+    @_delay_2_data ||= Async do
+      puts "getting delay_2_data"
+      JSON.parse(internet.get("https://httpbin.org/delay/2").read)
     end
   end
 end
