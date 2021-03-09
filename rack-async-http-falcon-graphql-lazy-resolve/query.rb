70a3529f3edbe5d7820a50c730144dbacc4cd8cf
Trevor Turk <trevorturk@gmail.com>
2021-03-09 11:33:31 -0600

    yield fixes it, but why

--- rack-async-http-falcon-graphql-lazy-resolve/query.rb
+++ rack-async-http-falcon-graphql-lazy-resolve/query.rb
@@ -6,32 +6,38 @@ class Query < GraphQL::Schema::Object
   field :three, String, null: false
 
   def one
-    delay_1_data["url"]
+    delay_1_data do |data|
+      data["url"]
+    end
   end
 
   def two
-    delay_2_data["url"]
+    delay_2_data do |data|
+      data["url"]
+    end
   end
 
   def three
-    delay_2_data["url"]
+    delay_2_data do |data|
+      data["url"]
+    end
   end
 
   def internet
     @_internet ||= Async::HTTP::Internet.new
   end
 
-  def delay_1_data
+  def delay_1_data(&block)
     @_delay_1_data ||= Async do
       puts "getting delay_1_data"
-      JSON.parse(internet.get("https://httpbin.org/delay/1").read)
+      yield JSON.parse(internet.get("https://httpbin.org/delay/1").read)
     end
   end
 
-  def delay_2_data
+  def delay_2_data(&block)
     @_delay_2_data ||= Async do
       puts "getting delay_2_data"
-      JSON.parse(internet.get("https://httpbin.org/delay/2").read)
+      yield JSON.parse(internet.get("https://httpbin.org/delay/2").read)
     end
   end
 end
