dd5da90d883fb7d328349162a7fc2dfa1eeed59f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-04-22 09:14:46 +1200

    Use shared internet instance. (#8)

--- rails-async-http-falcon-graphql-batch/app/models/async_loader.rb
+++ rails-async-http-falcon-graphql-batch/app/models/async_loader.rb
@@ -3,26 +3,29 @@ require "async/barrier"
 require "async/http/internet"
 require "kernel/sync"
 
+require 'thread/local'
+
+class Async::HTTP::Internet
+  extend Thread::Local
+end
+
 class AsyncLoader < GraphQL::Batch::Loader
   def perform(urls)
     Sync do
-      internet = Async::HTTP::Internet.new
       barrier = Async::Barrier.new
+      internet = Async::HTTP::Internet.instance
 
       urls.each do |url|
         barrier.async do
-          puts "AsyncHttp#get: #{url}"
+          Console.logger.info "AsyncHttp#get: #{url}"
           data = JSON.parse(internet.get(url).read)
           fulfill(url, data)
-          puts "AsyncHttp#fulfill: #{url}"
+          Console.logger.info "AsyncHttp#fulfill: #{url}"
         end
       end
 
-      puts "AsyncHttp#wait"
+      Console.logger.info "AsyncHttp#wait"
       barrier.wait
-    ensure
-      puts "AsyncHttp#close"
-      internet&.close
     end
   end
 end
