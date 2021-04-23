a9e7a195f81dd053d3f3e71d5953846369d80d95
Trevor Turk <trevorturk@gmail.com>
2021-04-23 09:37:45 -0500

    rack-async-http-falcon-gzip (#7)

---
+++ rack-async-http-falcon-gzip/README.md
@@ -0,0 +1,11 @@
+Example app using:
+  * rack
+  * falcon
+  * async-http
+
+Notes:
+  * Demo how to use `Accept-Encoding: gzip`
+
+ Running:
+
+    falcon serve --count 1
---
+++ rack-async-http-falcon-gzip/app.rb
@@ -0,0 +1,26 @@
+require "async"
+require "async/http/internet"
+
+class App
+  def self.call(env)
+    internet = Async::HTTP::Internet.new
+    url = "http://httpbin.org/gzip"
+
+    # headers can be a hash...
+    headers = {
+      "accept-encoding" => "gzip",
+      "user-agent" => "example",
+    }
+
+    # ...or an array...
+    headers = [
+      ["accept-encoding", "gzip"],
+      ["user-agent", "example"],
+    ]
+
+    response = internet.get(url, headers)
+    body = JSON.parse(response.read)
+
+    [200, {}, [body.to_json]]
+  end
+end
