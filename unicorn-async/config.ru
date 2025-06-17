70f4e34c9c572ad6aa7d8ac87fe7f0a25bc7b81c
Samuel Williams <samuel.williams@shopify.com>
2025-06-17 11:11:48 +0900

    More unicorn config.

--- unicorn-async/config.ru
+++ unicorn-async/config.ru
@@ -1,4 +1,2 @@
 
-require_relative "unicorn"
-
 run ->(env) {[200, {"content-type" => "text/plain"}, ["Hello, World!"]]}
--- unicorn-async/unicorn.rb
+++ unicorn-async/unicorn.rb
@@ -1,3 +1,12 @@
+ENV["RACK_ENV"] = "none"
+
+unicorn_workers = Integer(ENV.fetch("UNICORN_WORKERS", 2))
+worker_processes(unicorn_workers)
+preload_app(true)
+check_client_connection(true)
+timeout(30)
+listen(9292, backlog: unicorn_workers * 10)
+
 module AsyncUnicorn
   def worker_loop(worker)
     Sync do
