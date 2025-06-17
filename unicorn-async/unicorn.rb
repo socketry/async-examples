157138c73bd7e6db5e286dcd4ab872f8f8eb73f6
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-06-17 12:28:38 +0900

    Fix indentation.

--- unicorn-async/unicorn.rb
+++ unicorn-async/unicorn.rb
@@ -1,3 +1,22 @@
+
+module AsyncUnicorn
+	def worker_loop(worker)
+		Sync do
+			super
+		end
+	end
+end
+
+begin
+	require "async"
+	require "unicorn/http_server"
+	Unicorn::HttpServer.prepend(AsyncUnicorn)
+
+	warn "Running Unicorn with Async #{IO::Event::Selector.default}"
+rescue LoadError
+	warn "Async is not available. Please install the async gem."
+end
+
 ENV["RACK_ENV"] = "none"
 
 unicorn_workers = Integer(ENV.fetch("UNICORN_WORKERS", 2))
@@ -6,21 +25,3 @@ preload_app(true)
 check_client_connection(true)
 timeout(30)
 listen(9292, backlog: unicorn_workers * 10)
-
-module AsyncUnicorn
-  def worker_loop(worker)
-    Sync do
-      super
-    end
-  end
-end
-
-begin
-  require "async"
-  require "unicorn/http_server"
-  Unicorn::HttpServer.prepend(AsyncUnicorn)
-
-  warn "Running Unicorn with Async #{IO::Event::Selector.default}"
-rescue LoadError
-  warn "Async is not available. Please install the async gem."
-end
